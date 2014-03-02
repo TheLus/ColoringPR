61cd463d0b301cf1d9e145cc6e93627f003866ad
中橋雅弘 <nakahashimasahiro@nakahashimasahiro-no-MacBook-Air.local>
2014-03-02 17:00:58 +0900

    仕方ないのでクリックにイベントつけた

--- js/coloringpr.js
+++ js/coloringpr.js
@@ -1,4 +1,11 @@
-document.onload = start();
+document.onLoad = start();
+document.onclick = function () {
+  start();
+}
 function start() {
+  setTimeout(changeBGColor, 300);
+}
+function changeBGColor() {
   $(".commit").css("background", "skyblue");
+  console.log("foo");
 }
