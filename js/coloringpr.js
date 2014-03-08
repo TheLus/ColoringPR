2e46eaba103c4281cfd2e798d2c92a74dadfccf2
中橋雅弘 <nakahashimasahiro@nakahashimasahiro-no-MacBook-Air.local>
2014-03-08 14:57:07 +0900

    url判別処理追加

--- js/coloringpr.js
+++ js/coloringpr.js
@@ -1,11 +1,28 @@
-document.onLoad = start();
-document.onclick = function () {
-  start();
-}
-function start() {
-  setTimeout(changeBGColor, 300);
-}
-function changeBGColor() {
-  $(".commit").css("background", "skyblue");
-  console.log("foo");
+{
+  var PULL_REQUEST = "pull";
+
+  document.onLoad = start();
+  document.onclick = function () {
+    start();
+  }
+  function start() {
+    setTimeout(function () {
+      var state = checkURL(document.URL);
+      switch(state) {
+      case PULL_REQUEST
+        console.log("pull_req");
+        break;
+      default:
+        console.log("default");
+        break;
+      }, 500);
+  }
+
+  function changeBGColor() {
+    $(".commit").css("background", "skyblue");
+    console.log("foo");
+  }
+  function checkURL(url) {
+    return url.split("/")[5];
+  }
 }
