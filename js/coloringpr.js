40bb1f59f20530bb7f721935b387fb584a0c449f
中橋雅弘 <nakahashimasahiro@nakahashimasahiro-no-MacBook-Air.local>
2014-03-08 15:40:27 +0900

    関数を呼ぶように変更

--- js/coloringpr.js
+++ js/coloringpr.js
@@ -2,25 +2,26 @@
   var PULL_REQUEST = "pull";
 
   document.onLoad = start();
-  document.onclick = function () {
-    start();
-  }
+  document.onclick = start();
+
   function start() {
     setTimeout(function () {
       var state = checkURL(document.URL);
       switch(state) {
-      case PULL_REQUEST
-        console.log("pull_req");
-        break;
-      default:
-        console.log("default");
-        break;
-      }, 500);
+        case PULL_REQUEST:
+          console.log("pull_req");
+          changeBGColor();
+          break;
+        default :
+          console.log("default");
+          break;
+      }
+    }, 500);
   }
 
   function changeBGColor() {
+    console.log("changeBGColor");
     $(".commit").css("background", "skyblue");
-    console.log("foo");
   }
   function checkURL(url) {
     return url.split("/")[5];
