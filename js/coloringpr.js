d5c7c3409f8677759f323796097578356b078464
中橋雅弘 <nakahashimasahiro@nakahashimasahiro-no-MacBook-Air.local>
2014-03-08 15:49:43 +0900

    ページ判別方法変更

--- js/coloringpr.js
+++ js/coloringpr.js
@@ -6,11 +6,13 @@
 
   function start() {
     setTimeout(function () {
-      var state = checkURL(document.URL);
-      switch(state) {
+      var pageType = getPageType(document.URL);
+      var pageNum = getPageNum(document.URL);
+
+      switch(pageType) {
         case PULL_REQUEST:
           console.log("pull_req");
-          changeBGColor();
+          onOpenPullRequestPage();
           break;
         default :
           console.log("default");
@@ -19,11 +21,18 @@
     }, 500);
   }
 
+  function onOpenPullRequestPage() {
+    console.log("onOpenPullRequestPage");
+  }
+
   function changeBGColor() {
     console.log("changeBGColor");
     $(".commit").css("background", "skyblue");
   }
-  function checkURL(url) {
+  function getPageType(url) {
     return url.split("/")[5];
+  }
+  function getPageNum(url) {
+    return url.split("/")[6];
   }
 }
